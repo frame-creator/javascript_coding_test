1d57cda5e80f7ac4be6b6fe40884b00ba48e2ba3
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-03-25 21:52:32 +0900

    프로그래머스 3진수 문제 풀이

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function solution(n) {
+   let answer = 0;
+  let temp = '';
+  DFS(n){
+    if() 
+       else
+       
+  DFS(parseInt(n/3))
+  }
+  DFS(n);
+   temp+=String(n%3); 
+    answer.split('').reverse().join('');
+   
+    return answer;
+}
+         
+ function solution(n) {
+  let answer = 0;
+  let temp = '';
+  function DFS(n){
+    if(n===0) return;
+       else{
+       
+  DFS(parseInt(n/3));
+  temp+=String(n%3);        
+  }
+  }
+  DFS(n);
+ 
+    temp.split('').reverse().join('');
+    
+    return answer;
+}
