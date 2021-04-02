1b339bb330f9736886bc6d575254aea0ae4a6a1f
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-04-02 20:54:55 +0900

    [프로그래머스] 문자열 다루기 기본 문제 풀이 과정

--- string_num.js
+++ string_num.js
@@ -1,3 +1,15 @@
+---
+function solution(s) {
+    let answer = true;
+    for (let x of s) {
+     if (isNaN(x)) answer =false;
+    }
+     if (s.length !== 4 && s.length !== 6) {
+         let answer = false;   
+     }
+
+    return answer;
+}
 ///
 function solution(s) {
     let answer = false;
