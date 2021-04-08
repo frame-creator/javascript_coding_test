6b297540c696cad6c497fc5d263bbac8861d02b3
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-04-08 09:32:08 +0900

    [자바스크립트 알고리즘] 2진수 출력 문제

---
+++ twodigit.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = "";
+    function DFS(n) {
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n/2);
+      answer += String(n % 2);
+    
+    }
+    }
+    DFS(n);
+    return answer;
+}
