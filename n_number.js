bc8832fd7a2a157d37de0234783ffcc722f96915
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-04-24 14:03:18 +0900

    [프로그래머스] x만큼 간격이 있는 n개의 숫자 문제

--- n_number.js
+++ n_number.js
@@ -1,8 +1,9 @@
+///
 function solution(x, n) {
     let answer = [];
     answer.push(x);
     for (let i = 1; i< n; i++){
-       answer.push(answer[i-1] + x)
-    }
+       answer.push(answer[i-1] + x);
+     }
     return answer;
 }
