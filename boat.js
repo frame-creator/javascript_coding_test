ccc4d05f6c9dfcdcfacbb50024b5681f75059085
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-05-03 08:38:20 +0900

    [프로그래머스] 구명 보트 문제 풀이 과정

--- boat.js
+++ boat.js
@@ -40,4 +40,17 @@ function solution(people, limit) {
      }
     if(sum > 0) answer ++;
      return answer;
+    
+    
+    ///
+    function solution(people, limit) {
+    let answer = 0;
+    let rt = people.length - 1 ;
+    people.sort((a,b) => b-a);
+    for (let lt = 0; lt <=rt; lt++) {
+        if(people[lt] +people[rt] <= limit) rt--;
+        answer++;
+    }
+    return answer;
+}
 }
