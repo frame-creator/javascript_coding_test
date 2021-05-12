adf26aeb213d5df11417901e31db309cb2988565
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-05-13 08:36:39 +0900

    [프로그래머스] JadenCase 문자열 만들기 문제

---
+++ JadenCase.js
@@ -0,0 +1,10 @@
+///
+function solution(s) {
+    let answer = '';
+    answer += s[0].toUpperCase();
+    for (let i = 1; i< s.length; i++) {
+        if(s[i-1] === ' ') answer += s[i].toUpperCase();
+        else answer += s[i].toLowerCase();
+    }
+    return answer;
+}
