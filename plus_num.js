066f893767cf0220a4ca08b0cfcfffb3a86bc9d2
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-05-15 08:16:29 +0900

    [프로그래머스] 약수의 개수와 덧셈 문제

---
+++ plus_num.js
@@ -0,0 +1,9 @@
+///
+function solution(left, right) {
+    let answer = 0;
+    for (let i = left; i<right+1; i++) {
+        if(Math.sqrt(i)%1 === 0) answer -=i;
+        else answer += i;
+    }
+    return answer;
+}
